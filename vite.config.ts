464516611dccf6c4e4bb597ea4fa5f5cf9cdc94d
Ramisa Tabassum <0962220005101039@isu.ac.bd>
2026-07-05 21:35:52 +0600

    Update routing base to root domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    vite: {
-      base: '/My_Portfolio/', // <-- ADD THIS LINE HERE
-    }
   },
 });
